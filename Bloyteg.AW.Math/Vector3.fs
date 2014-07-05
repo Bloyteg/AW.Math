1c71bed27e942477e66259e79057294b03a4b98a
Mr. Byte <bytemr@gmail.com>
2014-07-05 00:02:27 -0600

    Implement more Vector3 functionality.

--- Bloyteg.AW.Math/Vector3.fs
+++ Bloyteg.AW.Math/Vector3.fs
@@ -14,30 +14,47 @@
 namespace Bloyteg.AW.Math
 
 module Vector3 = 
-    let inline pack x y z = 
+    let inline create x y z = 
         (^V : (static member Create : ^S * ^S * ^S -> ^V) (x, y, z))
 
-    let inline unpack vector =
+    let inline (|Vector3|) vector =
         let x = (^V : (member X : ^S) vector)
         let y = (^V : (member Y : ^S) vector)
         let z = (^V : (member Z : ^S) vector)
         (x, y, z)
 
-    let inline length vector : ^S = 
-        let (x, y, z) = unpack vector
-        sqrt ((x * x) + (y * y) + (z * z))
+    let inline add lhs rhs : ^V =
+        match (lhs, rhs) with
+        | Vector3(lhsX, lhsY, lhsZ), Vector3(rhsX, rhsY, rhsZ) -> create (lhsX + rhsX) (lhsY + rhsY) (lhsZ + rhsZ)
 
-    let inline normalize vector : ^V = 
-        let (x, y, z) = unpack vector
-        let length = length vector
-        pack (x/length) (y/length) (z/length)
+    let inline subtract lhs rhs : ^V =
+        match (lhs, rhs) with
+        | Vector3(lhsX, lhsY, lhsZ), Vector3(rhsX, rhsY, rhsZ) -> create (lhsX - rhsX) (lhsY - rhsY) (lhsZ - rhsZ)
+
+    let inline multiplyScalar vector (scalar: ^S) : ^V =
+        match vector with
+        | Vector3(x, y, z) -> create (x*scalar) (y*scalar) (z*scalar)
+
+    let inline divideScalar vector (scalar: ^S) : ^V =
+        match vector with
+        | Vector3(x, y, z) -> create (x/scalar) (y/scalar) (z/scalar)
+
+    let inline negate vector : ^V =
+        match vector with
+        | Vector3(x, y, z) -> create -x -y -z
+
+    let inline length vector : ^S = 
+        match vector with
+        | Vector3(x, y, z) -> sqrt ((x * x) + (y * y) + (z * z))
+
+    let inline normalize vector : ^V = divideScalar vector (length vector)
 
     let inline dot lhs rhs : ^S =
-        let (lhsX, lhsY, lhsZ) = unpack lhs
-        let (rhsX, rhsY, rhsZ) = unpack rhs
-        (lhsX * rhsX) + (lhsY * rhsY) + (lhsZ * rhsZ)
+        match (lhs, rhs) with
+        | Vector3(lhsX, lhsY, lhsZ), Vector3(rhsX, rhsY, rhsZ) -> (lhsX * rhsX) + (lhsY * rhsY) + (lhsZ * rhsZ)
 
     let inline cross lhs rhs : ^V =
-        let (lhsX, lhsY, lhsZ) = unpack lhs
-        let (rhsX, rhsY, rhsZ) = unpack rhs
-        pack ((lhsY * rhsZ) - (lhsZ * rhsY)) ((lhsZ * rhsX) - (lhsX * rhsZ)) ((lhsX * rhsY) - (lhsY * rhsX))
+        match (lhs, rhs) with
+        | Vector3(lhsX, lhsY, lhsZ), Vector3(rhsX, rhsY, rhsZ) -> create ((lhsY * rhsZ) - (lhsZ * rhsY)) ((lhsZ * rhsX) - (lhsX * rhsZ)) ((lhsX * rhsY) - (lhsY * rhsX))
+
+
--- Bloyteg.AW.Math/Vector3f.fs
+++ Bloyteg.AW.Math/Vector3f.fs
@@ -12,24 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 namespace Bloyteg.AW.Math
+
 open System.Runtime.InteropServices
 
 [<StructLayout(LayoutKind.Explicit)>]
 type Vector3f = 
-    { [<field: FieldOffset(0)>]  X : single
-      [<field: FieldOffset(4)>]  Y : single
-      [<field: FieldOffset(8)>] Z : single }
+    { [<FieldOffset(0)>] X : single
+      [<FieldOffset(4)>] Y : single
+      [<FieldOffset(8)>] Z : single }
     
-    static member Create(x: single, y: single, z: single) = { X = x; Y = y; Z = z; }
-
+    static member Create(x : single, y : single, z : single) = 
+        { X = x
+          Y = y
+          Z = z }
+    
     static member Zero = Vector3f.Create(0.0f, 0.0f, 0.0f)
-    
-    member this.Length: single = Vector3.length this
-    
-    member this.Normalized: Vector3f = Vector3.normalize this
-
-    member this.Dot(other : Vector3f): single = Vector3.dot this other
-
-    member this.Cross(other : Vector3f): Vector3f = Vector3.cross this other
-
+    static member (+) (lhs : Vector3f, rhs : Vector3f) : Vector3f = Vector3.add lhs rhs
+    static member (-) (lhs : Vector3f, rhs : Vector3f) : Vector3f = Vector3.subtract lhs rhs
+    static member (~-) (vector : Vector3f) : Vector3f = Vector3.negate vector
+    static member (*) (lhs : Vector3f, rhs : single) : Vector3f = Vector3.multiplyScalar lhs rhs
+    static member (*) (lhs : single, rhs : Vector3f) : Vector3f = Vector3.multiplyScalar rhs lhs
+    static member (/) (lhs : Vector3f, rhs : single) : Vector3f = Vector3.divideScalar lhs rhs
+    member this.Length : single = Vector3.length this
+    member this.Normalized : Vector3f = Vector3.normalize this
+    member this.Dot(other : Vector3f) : single = Vector3.dot this other
+    member this.Cross(other : Vector3f) : Vector3f = Vector3.cross this other
     override this.ToString() = System.String.Format("({0}, {1}, {2})", this.X, this.Y, this.Z)
